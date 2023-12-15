63c86110777743d1133ca49956978cddb293a1a6
Macbook <macbook@ZGZ-GRICO-XXYB3923.local>
2023-12-15 12:26:45 +0100

    First commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:astro/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {},
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+  },
+};
---
+++ tailwind.config.mjs
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+    extend: {
+      colors: {
+        'custom-gray': '#F4F4F4',
+        'custom-teal': '#6FC3BB',
+        'custom-blue': '#4DA7B1',
+        'custom-green': '#67BD9A',
+        'custom-dark-green': '#46A78B',
+        'custom-dark': '#1F1E40',
+      },
+      fontFamily: {
+        suprema: ['Suprema', 'sans-serif'],
+      },
+      fontSize: {
+        fontSize: {
+          'sm': '0.6875rem', // 11px
+          'base': '1rem',      // 16px
+          'lg': '1.4375rem', // 23px
+          'xl': '2rem',      // 32px
+          '2xl': '2.8125rem', // 45px
+          '3xl': '4rem',     // 64px
+          '4xl': '5.625rem', // 90px
+        },
+      },
+      lineHeight: {
+        'extra-loose': '1.414',
+      }
+    },
+  },
+	plugins: [],
+}
